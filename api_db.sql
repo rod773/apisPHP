db6d026f559cb153289ee2037bed43dd418a37a3
rod773 <104315743+rod773@users.noreply.github.com>
2023-06-27 12:29:09 +0200

    create table task

---
+++ api_db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `api_db`.`task` (
+`id` INT NOT NULL,
+`name` VARCHAR(128) NOT NULL,
+`priority` INT DEFAULT NULL,
+`is_completed` boolean NOT NULL DEFAULT false,
+PRIMARY KEY (`id`),
+INDEX(name));
